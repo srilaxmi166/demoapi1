0c59e70d85bb7e61d8167005adea3a1451102df7
srilaxmi166 <76096505+srilaxmi166@users.noreply.github.com>
2021-04-21 10:47:05 +0530

    Add files via upload

---
+++ WebApplication3/Controllers/StudentsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Context;
+using WebApplication3.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly CRUDContext _CRUDContext;
+
+        public ValuesController(CRUDContext CRUDContext)
+        {
+            _CRUDContext = CRUDContext;
+        }
+
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return _CRUDContext.Students;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            return _CRUDContext.Students.SingleOrDefault(x => x.StudentId == id);
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] Student student)
+        {
+            _CRUDContext.Students.Add(student);
+            _CRUDContext.SaveChanges();
+
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id,[FromBody] Student student)
+        {
+            student.StudentId = id;
+            _CRUDContext.Students.Update(student);
+            _CRUDContext.SaveChanges();
+
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var item = _CRUDContext.Students.FirstOrDefault(x => x.StudentId == id);
+            if(item!=null)
+            {
+                _CRUDContext.Students.Remove(item);
+                _CRUDContext.SaveChanges();
+             
+            }
+        }
+    }
+}
---
+++ WebApplication3/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StudentId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Roll { get; set; }
+    }
+}
